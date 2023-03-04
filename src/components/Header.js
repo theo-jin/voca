63f96d75d94f2120e9a4e7af83433e4ab6790ed0
theo <jhb_august@naver.com>
2023-03-04 21:23:55 +0900

    feat:header 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 export default function Header (){
+
   return (
   <div className="header">
     <h1>
-      <Link to="/"> 영단어 </Link>
+      <Link to="/"> VOCA </Link>
     </h1>
     <div className="menu">
         <Link to="/create_word" className="link">
