d8f29aef4021daecdcb41eb9b7edc48ec9ddff1d
theo <jhb_august@naver.com>
2023-03-06 22:06:07 +0900

    feat:css수정

--- src/common/components/Header.js
+++ src/common/components/Header.js
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 import classes from "./Header.module.css"
+
 export default function Header (){
 
   return (
-  <div className="header">
+  <div className={classes.header}>
     <h1>
-      <Link to="/"> VOCA </Link>
+      <Link className={classes.logo} to="/"> VOCA </Link>
     </h1>
     <div className="menu">
-        <Link to="/create_word" className="link">
+        <Link className={classes.btn} to="/create_word" className="link">
           단어 추가
         </Link>
-        <Link to="/create_day" className="link">
+        <Link className={classes.btn} Link to="/create_day" className="link">
           Day 추가
         </Link>
       </div>
